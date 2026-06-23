5a7d1244e2e31d5771a1d7832b6af185fc45e803
Pedro Marques <pedromarques391@gmail.com>
2026-06-23 15:37:39 -0300

    feat: add openAPI with auth router in API.

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,20 @@
+import { openapi } from "@elysia/openapi";
 import { Elysia } from "elysia";
-import { auth } from "./lib/auth";
+import { auth, OpenAPI } from "./lib/auth";
 
-const app = new Elysia().mount(auth.handler).listen(3000);
+const app = new Elysia()
+  .mount(auth.handler)
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    }),
+  )
+  .get("/", () => "Hello World!");
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
--- back-end/src/lib/auth.ts
+++ back-end/src/lib/auth.ts
@@ -1,10 +1,34 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { openAPI } from "better-auth/plugins";
 import { db } from "../db";
 
 export const auth = betterAuth({
-  basePath: "/",
+  basePath: "/auth",
+  plugins: [openAPI()],
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>;
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema());
+export const OpenAPI = {
+  getPaths: (prefix = "/auth") =>
+    getSchema().then(({ paths }) => {
+      const reference: typeof paths = Object.create(null);
+      for (const path of Object.keys(paths)) {
+        const key = prefix + path;
+        reference[key] = paths[path];
+        for (const method of Object.keys(paths[path])) {
+          const operation = (reference[key] as any)[method];
+          operation.tags = ["Better Auth"];
+        }
+      }
+      return reference;
+    }) as Promise<any>,
+  components: getSchema().then(({ components }) => components) as Promise<any>,
+} as const;
