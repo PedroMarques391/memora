d933b19bbd40d3a304ee0123c13bc72d05552e94
Pedro Marques <pedromarques391@gmail.com>
2026-06-22 23:50:32 -0300

    feat: finish better auth config.

--- back-end/src/lib/auth.ts
+++ back-end/src/lib/auth.ts
@@ -1,4 +1,10 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db";
+
 export const auth = betterAuth({
-  //...
+  basePath: "/",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
 });
