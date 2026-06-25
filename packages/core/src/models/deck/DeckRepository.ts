dac5b82fad05544e0374a5130a1a6d7f569fd88a
Pedro Marques <pedromarques391@gmail.com>
2026-06-25 20:30:26 -0300

    feat/core: create new method in repository interface.

--- packages/core/src/models/deck/DeckRepository.ts
+++ packages/core/src/models/deck/DeckRepository.ts
@@ -2,6 +2,7 @@ import type { CreateDeckProps, Deck } from "./Deck";
 
 export interface DeckRepository {
   findById(id: string): Promise<Deck | undefined>;
+  findByUserId(userId: string): Promise<Deck[]>;
   save(deck: Partial<Deck>, id: string): Promise<void>;
   remove(id: string): Promise<void>;
   findAll(): Promise<Deck[]>;
